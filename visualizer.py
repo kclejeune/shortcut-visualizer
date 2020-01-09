c885b9fd9407ce45600c4d3ed8f04d3663792c2b
Kennan LeJeune <kc.lejeune@gmail.com>
2020-01-09 04:31:52 -0600

    WIP: fix issues with left/right specific shortcuts

--- visualizer.py
+++ visualizer.py
@@ -6,6 +6,30 @@ import pync
 from pynput import keyboard
 
 
+def format_key(key: str) -> str:
+    """
+    Convert a key into the format necessary for keyboard.HotKey.parse()
+    e.g. ctrl -> <ctrl>, a -> a
+
+    Parameters:
+    ----------
+        key - the key to be converted
+    Returns:
+    --------
+        the formatted key
+    """
+    if "_" in key:
+        key = key[: key.index("_")]
+    return key if len(key) == 1 else f"<{key}>"
+
+
+def normalize(keys: list, modifiers: list = ["ctrl", "alt", "shift", "cmd"]) -> str:
+    keys.sort(
+        key=lambda k: (modifiers.index(k) if k in modifiers else len(modifiers), str(k))
+    )
+    return "+".join(format_key(key) for key in keys)
+
+
 def parse_hotkeys(
     file: str = argv[1] if len(argv) > 1 else os.path.realpath("hotkeys.json"),
 ) -> dict:
@@ -26,60 +50,45 @@ def parse_hotkeys(
     with open(file) as f:
         hotkeys = json.load(f)
         for hotkey in hotkeys:
-            hotkey["keys"] = set(hotkey["keys"])
+            hotkey["keys"] = normalize(hotkey["keys"])
     return hotkeys
 
 
-hotkeys = parse_hotkeys()
-
-
-def format_key(key: str) -> str:
-    """
-    Convert a key into the format necessary for keyboard.HotKey.parse()
-    e.g. ctrl -> <ctrl>, a -> a
-
-    Parameters:
-    ----------
-        key - the key to be converted
-    Returns:
-    --------
-        the formatted key
-    """
-    return key if len(key) == 1 else f"<{key}>"
-
-
-def parse_key(key: keyboard.Key) -> str:
-    """
-    extract the actual key value from a keyboard.Key object
-    """
-    try:
-        return f"{key.char}"
-    except AttributeError:
-        return f"{key.name}"
-
-
-def normalize_hotkey(hotkey: dict) -> str:
-    return "+".join(format_key(key) for key in hotkey["keys"])
-
-
-def send_notification(hotkey):
-    if not hotkeys.get(hotkey):
-        for hk in hotkeys:
-            if normalize_hotkey(hk) == hotkey:
-                hotkey = hk
-                break
+def handle_hotkey_press(hotkey: str):
+    print(hotkey)
     pync.notify(
-        title=hotkey["name"],
-        subtitle="+".join(hotkey["keys"]),
+        title=hotkey["keys"].replace("<", "").replace(">", ""),
+        subtitle=hotkey["name"],
         message=hotkey["description"],
         ignoreDnD=True,
         appIcon="",
     )
 
 
-global_hotkeys = {
-    normalize_hotkey(hotkey): lambda: send_notification(hotkey) for hotkey in hotkeys
-}
-print(global_hotkeys)
-with keyboard.GlobalHotKeys(global_hotkeys) as h:
-    h.join()
+def monitor_input():
+    def on_press(key):
+        try:
+            print("alphanumeric key {0} pressed".format(key.char))
+        except AttributeError:
+            print("special key {0} pressed".format(key))
+
+    def on_release(key):
+        print("{0} released".format(key))
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+
+if __name__ == "__main__":
+    global_hotkeys = {
+        hotkey["keys"]: lambda: handle_hotkey_press(hotkey)
+        for hotkey in parse_hotkeys()
+    }
+    global_hotkeys["<esc>"] = exit
+
+    with keyboard.GlobalHotKeys(global_hotkeys) as h:
+        h.join()
